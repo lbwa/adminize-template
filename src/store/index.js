de9c5fc0df45d6fe2d356dbe052b4a25a38f4e05
lbwa <32980314+lbwa@users.noreply.github.com>
2018-11-21 10:32:36 +0800

    feat(vuex): preserve all state when page reloading
    preserve all vuex state when page reloading

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
+// Preserve all vuex state when page reloading.
+import createPersistedState from 'vuex-persistedstate'
 import state from './state'
 import getters from './getter'
 // sync operation
@@ -10,6 +12,10 @@ import actions from './actions'
 import modules from './modules'
 
 const __DEV__ = process.env.NODE_ENV === 'development'
+const persistedState = createPersistedState({
+  key: '__VUEX_STORE__',
+  storage: window.sessionStorage // default: window.localStorage
+})
 
 Vue.use(Vuex)
 
@@ -20,5 +26,7 @@ export default new Vuex.Store({
   mutations,
   actions,
   strict: __DEV__,
-  plugins: __DEV__ ? [createLogger()] : []
+  plugins: __DEV__
+    ? [createLogger(), persistedState]
+    : [persistedState]
 })

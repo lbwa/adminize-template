80baa1c89cacd012e068ea0ebd8e91b8b9df7c8b
lbwa <mr_lbw@outlook.com>
2019-03-28 13:31:25 +0800

    chore: add error handler for error which has code

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 import { tokenFromStorage } from 'UTILS/storage'
 import store from 'STORE'
 import { Notification } from 'element-ui'
+import { eventBus } from 'UTILS'
+
+const h = eventBus.$createElement.bind(eventBus)
 
 /**
  * @description 创建一个 axios 基础请求实例，提示，环境变量可在 .env.[mode] files 中定义
@@ -33,15 +36,16 @@ export default function createBaseRequest (baseURL) {
   )
 
   request.interceptors.response.use(
-    ({ data, status, statusText }) => {
+    ({ data, status, statusText, config: { url } }) => {
       // response schema: https://github.com/axios/axios#response-schema
+      // 1. error capture for http code
       if (status !== 200) {
         return Promise.reject(
           new Error(`[response error]: ${status}, ${statusText}`)
         )
       }
 
-      // 1.1. access_token 过期
+      // 2. invalid access_token
       if (data.code === 3000) {
         // 触发 router 的 beforeEach 导航守卫中的 token 检测并重定向至 login 页
         tokenFromStorage.setItem('')
@@ -50,25 +54,26 @@ export default function createBaseRequest (baseURL) {
           .dispatch('login/userLogout')
           .then(() => Promise.reject(data))
       }
-      // 1.2 登录失败
-      if (data.code === 5000) {
-        return Promise.reject(data)
-      }
 
+      // 3. error capture (Only capture error which has a error code)
       if (data.code && data.code !== 2000) {
         Notification.error({
           title: 'Error',
-          message: 'Update data failed'
+          message: h(
+            'div',
+            data.code === 3002
+              ? [
+                'Unauthorized request',
+                h('div', { style: 'work-break: break-all' }, url)
+              ]
+              : 'Update data failed'
+          ),
+          position: 'bottom-right'
         })
         return Promise.reject(data)
       }
-      // 2. 其他错误
-      // ! 除非知道确切的错误 code，否则应谨慎处理其他非确定 code
-      // ! 如响应二进制下载数据时，正确的二进制数据将被 reject
-      // if (data.code !== 2000 && data.code !== 3000) {
-      //   return Promise.reject(data)
-      // }
-      // 3. 正常响应
+
+      // 4. normal response or binary data
       return data
     },
     err => {

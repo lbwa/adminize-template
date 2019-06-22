becaff749c9b90273ac5201f99d9e18be8c040e5
lbwa <mr_lbw@outlook.com>
2019-06-22 10:50:39 +0800

    refactor(page) rebuild private pages for more expressive

---
+++ src/pages/Private/Admin/index.vue
@@ -0,0 +1,20 @@
+<template functional>
+  <div class="admin">
+    <p>
+      Current route is
+      <strong>{{ parent.$route.path }}</strong>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Admin'
+}
+</script>
+
+<style lang="sass" scoped>
+.admin
+  +flex-center
+  height: 100%
+</style>
---
+++ src/pages/StaticPrivate/Admin/index.vue
@@ -0,0 +1,21 @@
+<template functional>
+  <div class="admin">
+    <p>
+      Current routes is
+      <strong>{{ parent.$route.path }}</strong
+      >.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Admin'
+}
+</script>
+
+<style lang="sass" scoped>
+.admin
+  +flex-center
+  height: 100%
+</style>

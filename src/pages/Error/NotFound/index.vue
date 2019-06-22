424f4f2fe529222d6654ad5acb14d1b73740b7de
lbwa <mr_lbw@outlook.com>
2019-06-22 10:09:33 +0800

    refactor(error): use funcitonal SFC to replace normal functional component

---
+++ src/pages/Error/NotFound/index.vue
@@ -0,0 +1,18 @@
+<template functional>
+  <section class="not-found">
+    <h2>404</h2>
+    <p>Seems nothing could found.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+
+<style lang="sass" scoped>
+.not-found
+  +flex-center
+  height: 100%
+</style>
---
+++ src/pages/Error/Unauthorized/index.vue
@@ -0,0 +1,19 @@
+<template functional>
+  <section class="unauthorized">
+    <h2>403</h2>
+    <p>You are unauthorized !</p>
+    <p>If you have any questions, contact tech support !</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Unauthorized'
+}
+</script>
+
+<style lang="sass" scoped>
+.unauthorized
+  +flex-center
+  height: 100%
+</style>
